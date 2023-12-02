249ec7543e2c1acf9ce0af2fd6d3bab07db4cc1f
Grzegorz Iwon <grzesiekiwon20@hotmail.com>
2023-12-02 11:03:52 +0000

    Added a ClientRequest and ClientResponse to achieve a clear separation of concerns. The ClientRequest class can be responsible for encapsulating all the information related to the request (e.g., endpoint, method, headers, payload), while the ClientResponse class can handle the response details (status code, headers, body)

---
+++ src/main/java/com/beautysalon/controller/dto/ClientRequest.java
@@ -0,0 +1,18 @@
+package com.beautysalon.controller.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ClientRequest {
+
+        private String name;
+        private String email;
+
+}
---
+++ src/main/java/com/beautysalon/controller/dto/ClientResponse.java
@@ -0,0 +1,21 @@
+package com.beautysalon.controller.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ClientResponse {
+
+        private Long id;
+        private String name;
+        private String email;
+        private List<Long> bookings;
+}
