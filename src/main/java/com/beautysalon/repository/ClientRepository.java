62056485d5df01b8ffedb9445d432fc2a4a2cde1
Grzegorz Iwon <grzesiekiwon20@hotmail.com>
2023-12-04 17:18:48 +0000

    feat: Add ClientRepository with query methods
    - Added ClientRepository interface to manage Client entities.
    - Included query methods to retrieve clients by email and name.
    - Added a pageable query method to retrieve a page of clients by name.
    - Added comments to document the purpose of each method.

--- src/main/java/com/beautysalon/repository/ClientRepository.java
+++ src/main/java/com/beautysalon/repository/ClientRepository.java
@@ -8,12 +8,34 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+/**
+ * Repository interface for managing Client entities.
+ */
 @Repository
 public interface ClientRepository extends ListCrudRepository<Client, Integer> {
 
+        /**
+         * Retrieve a client by email.
+         *
+         * @param email The email of the client.
+         * @return The client with the given email.
+         */
         Client findByEmail(String email);
+
+        /**
+         * Retrieve a list of clients by name.
+         *
+         * @param name The name of the client.
+         * @return List of clients with the given name.
+         */
         List<Client> findByName(String name);
 
+        /**
+         * Retrieve a page of clients by name.
+         *
+         * @param name     The name of the client.
+         * @param pageable The pagination information.
+         * @return Page of clients with the given name.
+         */
         Page<Client> findByName(String name, Pageable pageable);
 }
