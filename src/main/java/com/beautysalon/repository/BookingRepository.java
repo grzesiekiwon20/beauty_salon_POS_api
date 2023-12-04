2e1feddb081abb2e8a2cec8bb4b0c244d2b5ddc4
Grzegorz Iwon <grzesiekiwon20@hotmail.com>
2023-12-04 17:14:12 +0000

    feat: Add BookingRepository with query methods
    - Added BookingRepository interface to manage Booking entities.
    - Included query methods to retrieve bookings by ID, date, start time, end time, and service type.
    - Added comments to document the purpose of each method.

--- src/main/java/com/beautysalon/repository/BookingRepository.java
+++ src/main/java/com/beautysalon/repository/BookingRepository.java
@@ -1,10 +1,61 @@
 package com.beautysalon.repository;
 
 import com.beautysalon.repository.model.Booking;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 
+/**
+ * Repository interface for managing Booking entities.
+ */
 @Repository
-public interface BookingRepository extends CrudRepository<Booking, Integer> {
+public interface BookingRepository extends ListCrudRepository<Booking, Integer> {
+
+    /**
+     * Retrieve a list of bookings by ID.
+     *
+     * @param id The ID of the booking.
+     * @return List of bookings with the given ID.
+     */
+    @Query("SELECT b FROM Booking b WHERE b.id = ?1")
+    List<Booking> findById(int id);
+
+    /**
+     * Retrieve a booking by date.
+     *
+     * @param date The date of the booking.
+     * @return Optional containing the booking with the given date (if exists).
+     */
+    @Query("SELECT b FROM Booking b WHERE b.date = ?1")
+    Optional<Booking> findByDate(String date);
+
+    /**
+     * Retrieve a booking by start time.
+     *
+     * @param startTime The start time of the booking.
+     * @return Optional containing the booking with the given start time (if exists).
+     */
+    @Query("SELECT b FROM Booking b WHERE b.startTime = ?1")
+    Optional<Booking> findByStartTime(String startTime);
+
+    /**
+     * Retrieve a list of bookings by end time.
+     *
+     * @param endTime The end time of the booking.
+     * @return List of bookings with the given end time.
+     */
+    @Query("SELECT b FROM Booking b WHERE b.endTime = ?1")
+    List<Booking> findByEndTime(String endTime);
+
+    /**
+     * Retrieve a list of bookings by service type.
+     *
+     * @param serviceType The service type of the booking.
+     * @return List of bookings with the given service type.
+     */
+    @Query("SELECT b FROM Booking b WHERE b.serviceType = ?1")
+    List<Booking> findByServiceType(String serviceType);
 }
