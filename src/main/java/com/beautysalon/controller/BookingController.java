56a412a4ca2041dfa75073e81adfc2a597e6d723
Grzegorz Iwon <grzesiekiwon20@hotmail.com>
2023-12-12 12:25:11 +0000

    chore: Update BookingController with improved request parameters and service method names
    - In the findById endpoint, use Integer type for the 'id' parameter.
    - In the findByDate endpoint, use LocalDate type for the 'date' parameter.
    - In the findByServiceType endpoint, use ServiceType type for the 'serviceType' parameter.
    - Add a new endpoint findServiceByEmail to retrieve bookings by client email.
    - Update the method names in the BookingService accordingly for consistency.
    - Add comments for better code readability.

--- src/main/java/com/beautysalon/controller/BookingController.java
+++ src/main/java/com/beautysalon/controller/BookingController.java
@@ -2,12 +2,14 @@ package com.beautysalon.controller;
 
 
 import com.beautysalon.controller.dto.BookingRequest;
+import com.beautysalon.repository.model.ServiceType;
 import com.beautysalon.service.BookingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 
 
 @RestController
@@ -15,52 +17,44 @@ import org.springframework.web.bind.annotation.*;
 public class BookingController {
 
     private final BookingService service;
-
-    @GetMapping("/api/booking")
-    public ResponseEntity<?> findById(@RequestParam Long id) {
+    //Endpoint to find booking by ID
+    @GetMapping("/api/booking/id")
+    public ResponseEntity<?> findById(@RequestParam Integer id) {
         try {
-            return new ResponseEntity<>(service.findResponseById(id), HttpStatus.ACCEPTED);
+            return new ResponseEntity<>(service.find(id), HttpStatus.ACCEPTED);
 
         }catch (Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
-
+    //Endpoint to create booking
     @PostMapping("/api/booking")
     public void createBooking(@RequestBody BookingRequest booking) {
         service.save(booking);
     }
+    //Endpoint to find bookings by Date
     @GetMapping("/api/booking/date")
-    public ResponseEntity<?> findByDate(@RequestParam String date) {
+    public ResponseEntity<?> findByDate(@RequestParam LocalDate date) {
         try {
             return new ResponseEntity<>(service.findResponseByDate(date), HttpStatus.ACCEPTED);
         }catch (Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
             }
     }
-
-    @GetMapping("/api/booking/startTime")
-    public ResponseEntity<?> findByStartTime(@RequestParam String startTime) {
+    //Endpoint to find bookings by ServiceType
+    @GetMapping("/api/booking/serviceType")
+    public ResponseEntity<?> findByServiceType(@RequestParam ServiceType serviceType) {
         try {
-            return new ResponseEntity<>(service.findResponseByStartTime(startTime), HttpStatus.ACCEPTED);
+            return new ResponseEntity<>(service.findResponseByServiceType(serviceType), HttpStatus.ACCEPTED);
         }catch (Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @GetMapping("/api/booking/endTime")
-    public ResponseEntity<?> findByEndTime(@RequestParam String endTime) {
-       try {
-              return new ResponseEntity<>(service.findResponseByEndTime(endTime), HttpStatus.ACCEPTED);
-       }catch (Exception e){
-           return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-       }
-    }
-    @GetMapping("/api/booking/serviceType")
-    public ResponseEntity<?> findByServiceType(@RequestParam String serviceType) {
-        try {
-            return new ResponseEntity<>(service.findResponseByServiceType(serviceType), HttpStatus.ACCEPTED);
+    @GetMapping("api/booking/email")
+    public ResponseEntity<?> findServiceByEmail(String email){
+        try{
+            return new ResponseEntity<>(service.findByEmail(email), HttpStatus.ACCEPTED);
         }catch (Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
