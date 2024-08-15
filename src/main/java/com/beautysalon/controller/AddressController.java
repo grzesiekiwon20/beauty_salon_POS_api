4755d25fd0fcf1391a8798eb1311137ebf3941e4
Grzegorz Iwon <grzesiekiwon20@hotmail.com>
2024-08-15 13:00:10 +0200

    update:

--- src/main/java/com/beautysalon/controller/AddressController.java
+++ src/main/java/com/beautysalon/controller/AddressController.java
@@ -3,12 +3,14 @@ package com.beautysalon.controller;
 
 import com.beautysalon.controller.dto.AddressRequest;
 import com.beautysalon.service.impl.GeneralServiceImpl;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Address", description = "The Address Api")
 @RestController
 @RequestMapping("/api/address")
 @RequiredArgsConstructor
