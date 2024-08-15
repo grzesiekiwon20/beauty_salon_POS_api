31de717c267b62137747e2d47899ff662d3a83e4
Grzegorz Iwon <grzesiekiwon20@hotmail.com>
2024-08-15 12:48:26 +0200

    update: - ActivityRequest.java instead of BookingRequest.java with different entity relation with User.java
    - ActivityResponse.java.java instead of BookingResponse.java.java with different entity relation with User.java

--- src/main/java/com/beautysalon/controller/dto/ActivityRequest.java
+++ src/main/java/com/beautysalon/controller/dto/ActivityRequest.java
@@ -2,29 +2,30 @@ package com.beautysalon.controller.dto;
 
 
 import com.beautysalon.repository.model.ServiceType;
+import com.beautysalon.repository.model.User;
+import com.beautysalon.repository.model.UserType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.net.Inet4Address;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-@Builder
+
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class BookingResponse {
+public class ActivityRequest {
 
-    private Integer id;
     private LocalDate date;
     private LocalTime startTime;
     private LocalTime finishTime;
     private ServiceType serviceType;
-    private String clientEmail;
-    private String employeeName;
-    private Integer clientId;
-    private Integer employeeId;
-
+    private UserType userType;
+    private boolean taskDone;
+    private User user;
 
 }
--- src/main/java/com/beautysalon/controller/dto/ActivityResponse.java
+++ src/main/java/com/beautysalon/controller/dto/ActivityResponse.java
@@ -2,6 +2,8 @@ package com.beautysalon.controller.dto;
 
 
 import com.beautysalon.repository.model.ServiceType;
+import com.beautysalon.repository.model.User;
+import com.beautysalon.repository.model.UserType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,19 +12,19 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-
-@Data
 @Builder
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class BookingRequest {
+public class ActivityResponse {
+
+    private Long id;
 
     private LocalDate date;
     private LocalTime startTime;
     private LocalTime finishTime;
     private ServiceType serviceType;
-    private String clientEmail;
-    private String employeeName;
-    private Integer clientId;
-    private Integer employeeId;
+    private UserType userType;
+    private boolean taskDone;
+    private User user;
 }
