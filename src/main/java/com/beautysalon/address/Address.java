d29cc9b01969935dc573033df18afe5acd6c76e4
Grzegorz Iwon <grzesiekiwon20@hotmail.com>
2024-09-09 17:31:14 +0200

    update: -

---
+++ src/main/java/com/beautysalon/address/Address.java
@@ -0,0 +1,30 @@
+package com.beautysalon.address;
+
+
+import com.beautysalon.common.BaseEntity;
+import com.beautysalon.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+
+@Entity
+@SuperBuilder
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address extends BaseEntity {
+
+    private String street;
+    private String city;
+    private String postCode;
+    private AddressType addressType;
+
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users;
+
+}
