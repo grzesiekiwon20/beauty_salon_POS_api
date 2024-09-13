68874fefbd402e9294eba2d1498b3c3a57b2a295
Grzegorz Iwon <grzesiekiwon20@hotmail.com>
2024-09-13 09:51:51 +0200

    update: - deleted

---
+++ src/main/java/com/beautysalon/history/ActivityHistory.java
@@ -0,0 +1,29 @@
+//package com.beautysalon.history;
+//
+//import com.beautysalon.activity.Activity;
+//import com.beautysalon.common.BaseEntity;
+//import com.beautysalon.user.User;
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.ManyToOne;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.experimental.SuperBuilder;
+//
+//
+//@Getter
+//@Setter
+//@SuperBuilder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Entity
+//public class ActivityHistory extends BaseEntity {
+//
+//
+////    @ManyToOne
+////    @JoinColumn(name = "activity_id")
+////    private Activity activity;
+//}
