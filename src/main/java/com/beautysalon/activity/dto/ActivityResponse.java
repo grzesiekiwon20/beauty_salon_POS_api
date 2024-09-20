4c6d57b09e298a7f78d04df4d26f758fe0a1c4a3
Grzegorz Iwon <grzesiekiwon20@hotmail.com>
2024-09-20 06:38:18 +0200

    update: -

--- src/main/java/com/beautysalon/activity/dto/ActivityResponse.java
+++ src/main/java/com/beautysalon/activity/dto/ActivityResponse.java
@@ -1,13 +1,10 @@
 package com.beautysalon.activity.dto;
 
-import com.beautysalon.type.Type;
-import com.beautysalon.user.User;
-import com.beautysalon.employee.Employee;
+
 import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
 
 @Builder
 @AllArgsConstructor
