9bd1b47c269b1b0a0fda8680437592141678fff5
Grzegorz Iwon <grzesiekiwon20@hotmail.com>
2024-09-20 06:35:44 +0200

    update: -

--- src/main/java/com/beautysalon/activity/dto/ActivityRequest.java
+++ src/main/java/com/beautysalon/activity/dto/ActivityRequest.java
@@ -1,15 +1,10 @@
 package com.beautysalon.activity.dto;
 
-import com.beautysalon.type.Type;
-import com.beautysalon.user.User;
-import com.beautysalon.employee.Employee;
-import lombok.Builder;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 
-@Builder
 public record ActivityRequest(
         Long id,
         LocalDate date,
