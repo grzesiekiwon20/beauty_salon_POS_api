4a899614b7303dc24be823b8c0b3f360f2e6a732
grzegorziwon <grzesiekiwon20@hotmail.com>
2026-01-09 12:01:04 +0100

    -update

--- src/main/java/com/beautysalon/order/Order.java
+++ src/main/java/com/beautysalon/order/Order.java
@@ -3,19 +3,23 @@ package com.beautysalon.order;
 
 import com.beautysalon.common.BaseEntity;
 import com.beautysalon.orderItem.OrderItem;
-import com.beautysalon.payment.Payment;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 
+import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Table(name = "customer_order")
 @EntityListeners(AuditingEntityListener.class)
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order extends BaseEntity {
 
     @Email
@@ -27,71 +31,11 @@ public class Order extends BaseEntity {
 
     private LocalDate orderDate;
 
-    @OneToOne
-    @JoinColumn(name = "payment_id")
-    private Payment payment;
+//    @OneToOne
+//    @JoinColumn(name = "payment_id")
+//    private Payment payment;
 
     private Double totalAmount;
     private OrderStatus orderStatus;
 
-    public Order(Long id, LocalDateTime createdDate, LocalDateTime lastModifiedDate, String createdBy, String lastModifiedBy, String email, List<OrderItem> orderItems, LocalDate orderDate, Payment payment, Double totalAmount, OrderStatus orderStatus) {
-        super(id, createdDate, lastModifiedDate, createdBy, lastModifiedBy);
-        this.email = email;
-        this.orderItems = orderItems;
-        this.orderDate = orderDate;
-        this.payment = payment;
-        this.totalAmount = totalAmount;
-        this.orderStatus = orderStatus;
-    }
-
-    public Order() {
-    }
-
-    public @Email String getEmail() {
-        return email;
-    }
-
-    public void setEmail(@Email String email) {
-        this.email = email;
-    }
-
-    public List<OrderItem> getOrderItems() {
-        return orderItems;
-    }
-
-    public void setOrderItems(List<OrderItem> orderItems) {
-        this.orderItems = orderItems;
-    }
-
-    public LocalDate getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(LocalDate orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public Payment getPayment() {
-        return payment;
-    }
-
-    public void setPayment(Payment payment) {
-        this.payment = payment;
-    }
-
-    public Double getTotalAmount() {
-        return totalAmount;
-    }
-
-    public void setTotalAmount(Double totalAmount) {
-        this.totalAmount = totalAmount;
-    }
-
-    public OrderStatus getOrderStatus() {
-        return orderStatus;
-    }
-
-    public void setOrderStatus(OrderStatus orderStatus) {
-        this.orderStatus = orderStatus;
-    }
 }
