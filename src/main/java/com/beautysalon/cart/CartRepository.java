d4171aef6125b2fb45a60a5e74903263de7e96a8
grzegorziwon <grzesiekiwon20@hotmail.com>
2026-01-09 11:48:51 +0100

    -update

--- src/main/java/com/beautysalon/cart/CartRepository.java
+++ src/main/java/com/beautysalon/cart/CartRepository.java
@@ -2,23 +2,10 @@ package com.beautysalon.cart;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, String> {
-
-
-
-//    @Query("""
-//select c from Cart c where c.sessionId=:sessionId
-//""")
-//    Cart findBySessionId(@Param("sessionId") String sessionId);
 
 }
